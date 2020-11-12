267a6ca68c0d3288913957d81208f5b249f8ccd3
Guillermo <gs.vera92@gmail.com>
2020-11-11 23:39:41 -0500

    comit del pelado

--- resources/views/memo.blade.php
+++ resources/views/memo.blade.php
@@ -1,14 +1,8 @@
 @extends('layouts.app')
 
-<<<<<<< HEAD
+
 @sectio('content')
-<<<<<<< HEAD
-    <h1>Titulo de memo</h1>
-=======
-@section('content')
-    <h1>Titulo garkencio</h1>
->>>>>>> 141f1bd08b6e99687110cce3a09ea30f6ee3aced
-=======
-    <h1>Titulo Garka</h1>
->>>>>>> d70978577bffa378935fa266f4ecb18115d62981
+
+    <h1>Titulo de pelado</h1>
+
 @endsection
