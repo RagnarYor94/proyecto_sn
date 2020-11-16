a1cd3740bfeabf0625b79e496c2251f93254b303
Guillermo <gs.vera92@gmail.com>
2020-11-16 13:28:58 -0500

    x

--- resources/views/catalogoHoteles/cadena.blade.php
+++ resources/views/catalogoHoteles/cadena.blade.php
@@ -26,12 +26,12 @@
     <div class="col m-1" >
         <div class="row">
         <div class="card border-dark col" style="max-width: 14rem;padding:0;">
-        <div class="card-header  bg-secondary text-white">Nombre de la Cadena</div>
+        <div class="card-header  bg-secondary text-white">Nombre de la Cadena {{$item->id}}</div>
         <div class="card-body text-dark">
             <h5 class="card-title">{{$item->cadena_hotelera}}</h5>
-            <a href="{{route('catalogoHoteles.editarCadena',$item)}}">
-            <button class="btn btn-success">Editar</button>
-            </a>
+            <!-- <a href="{{route('catalogoHoteles.editarCadena',$item)}}"> -->
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="editarCadena" data-whatever="@mdo">Editar</button>
+            <!-- </a> -->
             <form action="{{route('eliminarCadena',$item)}}" method="POST" class="d-inline">
             @method('DELETE')
             @csrf
@@ -43,6 +43,48 @@
     </div>
     @endforeach
     </div>
+
+
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+
+<div class="modal fade" id="editarCadena" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
     
     {{$cadenas->links()}}
     
